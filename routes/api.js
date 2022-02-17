42498cae33e9207638093dc6f3406f2d041ead30
Justyna <justyna.chmielinska@coderscrew.pl>
2022-02-17 20:25:32 +0100

    refactor: tidying repository structure

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controllers = require('../controllers/api/test')
-
-router.get('/', controllers.homepage);
-
-module.exports = router;

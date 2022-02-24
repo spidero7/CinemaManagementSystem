40269c930d8d8c3c2a3baa69aecbfc005a09e794
spidero7 <pajak.bartlomiej97@gmail.com>
2022-02-24 09:13:59 +0100

    fix(auth.js): add line to save the user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,6 +25,7 @@ router.post('/register', async (req, res) => {
 		password: hashedPassword,
 	})
 	try {
+		const savedUser = await user.save()
 		res.send({ user: user._id })
 	} catch (err) {
 		res.status(404).send(err)

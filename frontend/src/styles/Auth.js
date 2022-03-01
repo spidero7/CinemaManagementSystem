29b7b2e51b4fc623d6be477cad03bc72b6eef7df
spidero7 <pajak.bartlomiej97@gmail.com>
2022-03-01 17:07:54 +0100

    build: add styles

---
+++ frontend/src/styles/Auth.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FieldError = styled.span`
+	color: red;
+	font-size: 15px;
+	min-height: 18px;
+`;
+
+export const FormSuccess = styled.span`
+	color: green;
+	font-size: 12px;
+	min-height: 20px;
+`;

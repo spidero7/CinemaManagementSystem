8c998425b60f78b0c74b81fff783c7837b6e6e5a
spidero7 <pajak.bartlomiej97@gmail.com>
2022-03-02 08:08:38 +0100

    fix index.js

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,9 @@ const { port } = require('./config')
 //routes
 const apiRouter = require('./routes/api')
 const authUser = require('./routes/auth')
-<<<<<<< HEAD
 const accountRoute = require('./routes/myAccount')
-=======
 const postRoute = require('./routes/myAccount')
 const cinemaRoute = require('./routes/cinema')
->>>>>>> 48f25a538ecf8b4303553bf1e384ffb2dcda8e1e
 
 //database
 require('./db/mongoose')
@@ -22,12 +19,9 @@ app.use(express.json())
 //route middlewares
 app.use('/', apiRouter)
 app.use('/', authUser)
-<<<<<<< HEAD
 app.use('/', accountRoute)
-=======
 app.use('/', postRoute)
 app.use('/', cinemaRoute)
->>>>>>> 48f25a538ecf8b4303553bf1e384ffb2dcda8e1e
 
 //server
 app.listen(port, () => {

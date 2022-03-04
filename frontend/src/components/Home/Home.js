4a83618a427153f392ba8d0aa0a9b8aadda99cc9
spidero7 <pajak.bartlomiej97@gmail.com>
2022-03-04 14:09:47 +0100

    fix(Home.js): resolve conflict

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,5 @@
 const Home = () => {
-	return (
-		<div className="outer-container">
-			<div className="inner-container">
-				<h1>Home Page</h1>
-			</div>
-		</div>
-	);
+	return <div className="card">Home Page</div>;
 };
 
 export default Home;

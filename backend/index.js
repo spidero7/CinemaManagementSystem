fa9798bdcce20562aa36c2ca401390ee857cddf1
spidero7 <pajak.bartlomiej97@gmail.com>
2022-03-08 13:03:13 +0100

    build: add routes and middlewares in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,35 @@ const express = require('express')
 const app = express()
 const { port } = require('./config')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const cookieParser = require('cookie-parser')
+const credentials = require('./middleware/credentials')
 
 //routes
 const apiRouter = require('./routes/api')
-const authUser = require('./routes/auth')
-const accountRoute = require('./routes/myAccount')
+const registerRoute = require('./routes/register')
+const loginRoute = require('./routes/login')
+const refreshRoute = require('./routes/refreshToken')
+const logutRoute = require('./routes/logout')
+const myAccountRoute = require('./routes/myAccount')
 
 //database
 require('./db/mongoose')
 
 //middlewares
+app.use(credentials)
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3001' }))
+app.use(cookieParser())
 
 //route middlewares
 app.use('/', apiRouter)
-app.use('/', authUser)
-app.use('/', accountRoute)
+app.use('/register', registerRoute)
+app.use('/login', loginRoute)
+app.use('/refresh', refreshRoute)
+app.use('/logout', logutRoute)
+app.use('/my-account', myAccountRoute)
 
 //server
 app.listen(port, () => {

67ff366017ebc15b1d898048798f4afe5f44ee00
spidero7 <pajak.bartlomiej97@gmail.com>
2022-03-08 13:29:02 +0100

    fix: handle error

--- backend/routes/refreshToken.js
+++ backend/routes/refreshToken.js
@@ -10,10 +10,11 @@ router.get('/', async (req, res) => {
 	const refreshToken = cookies.jwt
 
 	const foundUser = await User.findOne({ refreshToken }).exec()
-	if (!foundUser) return res.sendStatus(403) //Forbidden
+	if (!foundUser) return res.send('No user in DB').status(403) //Forbidden
 	// evaluate jwt
-	jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-		if (err || foundUser._id !== decoded._id) return res.sendStatus(403)
+	jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err) => {
+		if (err) return res.sendStatus(403)
+
 		const accessToken = jwt.sign(
 			{ _id: foundUser._id },
 			process.env.ACCESS_TOKEN_SECRET,

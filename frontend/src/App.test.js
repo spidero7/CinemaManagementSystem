7409d91f9532926c76b1fb7b9f413a4af26ac3d6
spidero7 <pajak.bartlomiej97@gmail.com>
2022-03-20 08:34:36 +0100

    fix: failed test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,14 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthProvider';
 
-test('renders learn react link', () => {
-	render(<App />);
+test('renders App component', () => {
+	render(
+		<BrowserRouter>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</BrowserRouter>
+	);
 });

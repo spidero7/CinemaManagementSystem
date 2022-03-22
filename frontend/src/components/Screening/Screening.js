b48a4ecde37704079405758051f61f8961e74085
itspatys <patryk.pierzchaa@gmail.com>
2022-03-22 18:16:28 +0100

    Sync changes 2

--- frontend/src/components/Screening/Screening.js
+++ frontend/src/components/Screening/Screening.js
@@ -1,24 +1,40 @@
 import { useEffect, useState } from "react"
-import { DataTable } from 'primereact/datatable';
+// import { DataTable } from 'primereact/datatable';
+import { Dropdown } from 'primereact/dropdown';
+import { Toolbar } from 'primereact/toolbar';
 import useAxiosPrivate from "../../hooks/useAxiosPrivate"
 
 const Screening = () => {
     const axiosPrivate = useAxiosPrivate()
 
-    const [cinemas, setCinemas] = useState()
+    const [cinemas, setCinemas] = useState([])
+    const [selectedCinema, setSelectedCinema] = useState("")
+    const cities = [
+        { name: 'New York', code: 'NY' },
+    ];
+
+    const handleCinemaChange = () => {
+
+    }
 
     useEffect(() => {
         axiosPrivate.get('http://localhost:3000/cinemas')
         .then(response => setCinemas(response.data))
     },[])
+    
     return (
         <div>
-            <DataTable value={cinemas} responsiveLayout="scroll">
-                <Column field="code" header="Code"></Column>
-                <Column field="name" header="Name"></Column>
-                <Column field="category" header="Category"></Column>
-                <Column field="quantity" header="Quantity"></Column>
-            </DataTable>
+            <div>
+                <Toolbar left={<>
+                    <Dropdown 
+                        value={selectedCity1} 
+                        options={cinemas} 
+                        onChange={onCityChange} 
+                        optionLabel="cinema" 
+                        placeholder="Select cinema" 
+                    />
+                </>} right={<></>} />
+            </div>
         </div>
     )
 }
